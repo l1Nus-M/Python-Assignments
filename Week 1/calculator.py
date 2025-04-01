5e8435f1ed95c86ca0e4ab5da1b98f5123c88f7d
Linus Msuri <razer5854@gmail.com>
2025-04-01 12:08:18 +0300

    Week 1 Python Assignment Submission

---
+++ Week 1/calculator.py
@@ -0,0 +1,23 @@
+# User input
+num1 = float(input("Enter First number: "))
+num2 = float(input("Enter Second number: "))
+operation = input("Enter an Operator (+, -, *, /): ")
+
+# Calculation logic
+if operation == "+":
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif operation == "-":
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operation == "*":
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operation == "/":
+    if num2 != 0:  # Prevent division by zero
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Error: Division by zero is not allowed.")
+else:
+    print("Invalid operation. Please enter +, -, *, or /.")
